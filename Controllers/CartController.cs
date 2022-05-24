3c7ba35f8c59c8858a447af925004d61c73a51b9
CorinaNeatu <102432020+CorinaNeatu@users.noreply.github.com>
2022-05-24 10:16:29 +0300

    Add files via upload

---
+++ Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using library.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private readonly BookService _bookService;
+
+        public CartController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<app.Models.Book>? objBookList = _bookService.GetBooks();
+            return View(objBookList);
+        }
+    }
+}
